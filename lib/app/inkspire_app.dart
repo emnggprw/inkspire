309541f7c41638f2696ccc5d48550b102a08b95a
Eko <eko.mnggprw@gmail.com>
2025-04-03 00:42:11 +0700

    Fixed state inconsistencies due to duplicate instances of providers

--- lib/app/inkspire_app.dart
+++ lib/app/inkspire_app.dart
@@ -2,54 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:inkspire/presentation/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:inkspire/providers/theme_provider.dart';
-import 'package:inkspire/providers/chat_provider.dart';
 
 class InkSpireApp extends StatelessWidget {
   const InkSpireApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => ThemeProvider()),
-        ChangeNotifierProvider(create: (context) => ChatProvider()),
-      ],
-      child: Builder(
-        builder: (context) {
-          final themeProvider = Provider.of<ThemeProvider>(context);
+    final themeProvider = Provider.of<ThemeProvider>(context);
 
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'InkSpire',
-            themeMode: themeProvider.themeMode,
-            theme: ThemeData(
-              brightness: Brightness.light,
-              scaffoldBackgroundColor: Colors.white,
-              textTheme: const TextTheme(
-                bodyLarge: TextStyle(color: Colors.black, fontSize: 18),
-              ),
-              appBarTheme: const AppBarTheme(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
-                elevation: 0,
-              ),
-            ),
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-              scaffoldBackgroundColor: Colors.black,
-              textTheme: const TextTheme(
-                bodyLarge: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-              appBarTheme: const AppBarTheme(
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
-                elevation: 0,
-              ),
-            ),
-            home: const HomeScreen(), // No need to pass providers manually
-          );
-        },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'InkSpire',
+      themeMode: themeProvider.themeMode,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black, fontSize: 18),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+        ),
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+      ),
+      home: const HomeScreen(), // No need to pass providers manually
     );
   }
 }
